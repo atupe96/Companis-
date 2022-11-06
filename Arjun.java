740b568eaf8e2e8efe6e79a27bff6d4346778bf9
Arjun <atupe96@gmail.com>
2022-11-06 12:35:53 +0530

    recover

--- Arjun.java
+++ Arjun.java
@@ -1,4 +1,12 @@
+<<<<<<< Updated upstream
 public class methode(){
 
 System.out.prinltn("Methode")
 }
+=======
+public class Companies{
+
+public static void main (String [args] ){
+
+System.out.println("Companies");
+>>>>>>> Stashed changes
